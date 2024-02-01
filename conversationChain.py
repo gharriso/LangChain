273b85d689709e9ba5a408c500cb3e12c023a8d0
Guy Harrison <guy.a.harrison@gmail.com>
2024-02-02 10:45:57 +1100

    wip

---
+++ conversationChain.py
@@ -0,0 +1,11 @@
+from langchain.chains import ConversationChain
+from langchain_openai import OpenAI
+chatbot_llm = OpenAI(model_name='gpt-3.5-turbo-instruct')
+chatbot = ConversationChain(llm=chatbot_llm , verbose=False)
+while True:
+    human=input('Human: ')
+    if not human:
+        break
+    aiOutput=chatbot.predict(input=human)
+    print(aiOutput) 
+ 
